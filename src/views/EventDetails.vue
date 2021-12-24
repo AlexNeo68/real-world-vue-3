d614fb85f4191485004959e82ee12eb59e47be64
alexneo68 <tunning_brain@mail.ru>
2021-12-24 08:50:08 +0300

    l5

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="event">
+    <h1>{{event.title}}</h1>
+    <p>@{{event.date}} at {{event.time}} on {{event.location}}</p>
+    <p>{{event.description}}</p>
+  </div>
+</template>
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((res) => (this.event = res.data))
+      .catch((err) => console.log(err))
+  },
+}
+</script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -15,7 +15,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-
+import EventService from '@/services/EventService.js'
 export default {
   name: 'EventList',
   components: {
@@ -25,6 +25,13 @@ export default {
     return {
       events: null,
     }
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => {
+        this.events = res.data
+      })
+      .catch((err) => console.log(err))
   },
 }
 </script>
